7c44dbc96850f0c54e018bd0560bafcd87d9bf94
hasithafjhhe <118114873+hasithafjhhe@users.noreply.github.com>
2022-11-13 19:10:58 +0530

    My first commit
    My first commit

---
+++ temperature-calculator.py
@@ -0,0 +1,16 @@
+print("""- Converting the Fahrenheit Celsius  -
+Convert Celsius to Fahrenheit: type-F
+Convert Fahrenheit to Celsius: type-C""")
+
+inp = input("What do you want: ").upper()
+if inp == "F":
+    fahrenheit = float(input("Enter the temperature in Celsius: "))
+    f = (fahrenheit * 1.8) + 32
+    print("Fahrenheit: ", f)
+elif inp == "C":
+    celsius = float(input("Enter the temperature in Fahrenheit: "))
+    c = (celsius - 32) * 5 / 9
+    print("Celsius: ", c)
+else:
+    print("Please check the character you entered (Also F or C)")
+print("-", " ", "END", " ", "-")
